32e54db192bf57d60fc20e75a062457dd4896468
Bradley Reed <shenmusha@yahoo.com>
2021-09-01 15:42:19 -0400

    Mongoose examples part 1

---
+++ models/campsite.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String, 
+        require: true, 
+        unique: true
+    }, 
+    description:{
+        type: String, 
+        requires: true
+    }
+},
+{
+    timestamps: true
+});
+
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+module.exports = Campsite;
